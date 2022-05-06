284f88548eda1272ec796b8a7748a8d9f472d6a5
aram1l7 <arammartirosyann16@gmail.com>
2022-05-06 11:36:03 +0400

    fix

--- client/build/src/entry.jsx
+++
@@ -1,8 +0,0 @@
-// @flow
-import React from "react";
-import ReactDOM from "react-dom";
-import Animation from "./components/Animation";
-import "./modules/article";
-import "./modules/index";
-import './modules/fetchTweets'
-ReactDOM.render(<Animation />, document.getElementById("root"));
