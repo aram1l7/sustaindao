7ac8cb32348c9ff8a3284ec2073cb639a202beb0
aram1l7 <arammartirosyann16@gmail.com>
2022-05-06 15:34:57 +0400

    fix v99

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path');
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const axios = require('axios');
+require("dotenv").config();
+const app = express();
 app.use(cors());
-const twitter = require('./api/twitter');
-const PORT = process.env.PORT || 4000
-let origin = '*'
-app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin', origin)
-    res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With,Content-Type,Accept,Key,Access-Control-Allow-Headers,Cache-Control,Authorization")
-    res.header('Access-Control-Allow-Credentials', 'true')
-    next()
-})
 
-app.use('/api', twitter)
+const PORT = process.env.PORT || 4000;
+let origin = "*";
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type,Accept,Key,Access-Control-Allow-Headers,Cache-Control,Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "./client/_build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/_build/index.html"), (err) => {
@@ -26,7 +28,40 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/api/twitter/sustaindao", async (req, res) => {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+    },
+  };
+  axios
+    .get(
+      "https://api.twitter.com/2/users/1504551646608728064?user.fields=name,username,profile_image_url",
+      options
+    )
+    .then((response) => {
+      return res.json(response.data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    }); 
+});
 
-app.listen(PORT)
+app.get('/api/tweets', async (req,res) => {
+  const options = {
+      headers:{
+        Authorization:`Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+      }
+    }
+
+    axios.get("https://api.twitter.com/2/users/1504551646608728064/tweets?max_results=5&tweet.fields=created_at,text,public_metrics", options)
+    .then(response => {
+        return res.json(response.data.data)
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+app.listen(PORT);
 
 module.exports = app;
